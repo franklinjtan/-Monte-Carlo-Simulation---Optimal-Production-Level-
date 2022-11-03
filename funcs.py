d2acf0a91ea507a4f1aaa0187494362dda89e851
Franklin Tan <ft229@cornell.edu>
2022-11-03 15:24:56 -0400

    Added doc string and parameters

--- funcs.py
+++ funcs.py
@@ -2,3 +2,20 @@
 Author: GROUP 10
 Date: 11/03/22
 """
+
+def optimal_production_simulation(retail_price, production_cost, disposal_cost, number_to_manufacture):
+    """
+    Uses inputs to calculate the optimal production level given randomly generated demand
+
+    Parameter retail_price: the price that the product is being sold for
+    Precondition: retail_price is an integer >= $0.00
+
+    Parameter production_cost: the costs associated with manufacturing the product
+    Precondition: production_cost is an integer >= $0.00
+
+    Parameter disposal_cost: the costs associated with disposing of the product in an environmentally sustainable way
+    Precondition: disposal_cost is an integer >= $0.00
+
+    Parameter number_to_manufacture: the number that is manufactured as a result of the inputs
+    Precondition: number_to_manufacture is an integer >= 0
+    """
