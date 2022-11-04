77b2e4a34ec21dd67d4fbfec705badf65c08b176
Franklin Tan <ft229@cornell.edu>
2022-11-04 12:34:11 -0400

    Added function generate_random_demand

--- funcs.py
+++ funcs.py
@@ -2,8 +2,26 @@
 Author: GROUP 10
 Date: 11/03/22
 """
+import numpy as np
 
-def optimal_production_simulation(retail_price, production_cost, disposal_cost, number_to_manufacture):
+def generate_random_demand(mean, stddev, size):
+    """
+    Generates a random sample of demand instances from a normal (Gaussian) distribution.
+
+    Parameter mean: the average demand for the product
+    Precondition: mean is an integer
+
+    Parameter stddev: the standard deviation
+    Precondition: stddev is an integer
+
+    Parameter size: the number of instances or random samples to be created
+    Precondition: size is an integer
+    """
+    demand = np.random.normal(mean, stddev, size)
+    return demand
+
+
+def optimal_production_simulation(mean, stddev, size, retail_price, production_cost, disposal_cost, number_to_manufacture):
     """
     Uses inputs to calculate the optimal production level given randomly generated demand
 
@@ -19,3 +37,5 @@ def optimal_production_simulation(retail_price, production_cost, disposal_cost, 
     Parameter number_to_manufacture: the number that is manufactured as a result of the inputs
     Precondition: number_to_manufacture is an integer >= 0
     """
+
+# print(generate_random_demand(150, 20, 1000))
