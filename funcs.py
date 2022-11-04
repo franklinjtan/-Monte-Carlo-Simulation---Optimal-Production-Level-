51ddd8f7d8ffd8385b37ccff9dd440dd3d9458ab
Franklin Tan <ft229@cornell.edu>
2022-11-04 15:15:46 -0400

    Added func: plotting results

--- funcs.py
+++ funcs.py
@@ -3,6 +3,7 @@ Author: GROUP 10
 Date: 11/04/22
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 def generate_random_demand(mean, stddev, size):
     """
@@ -57,21 +58,31 @@ def optimal_production_simulation(mean, stddev, size, retail_price, production_c
         # Scenario where demand and units manufactured are equal
             profit_dict[demand[x]] = (units_manufactured * profit)
     # print(profit_dict) # Checking what the profit dictionary looks like
-    print(str(units_manufactured) + " units manufactured")
+    # print(str(units_manufactured) + " units manufactured")
 
     # Calculate mean profit of the production level
     sum = 0
     for val in profit_dict.values():
         sum += val
     result = round(sum / len(profit_dict),2)
-    print("AVG: " + str(result))
+    # print("AVG: " + str(result))
 
     # Calculate standard deviation of the profit
     profit_list = []
     for val in profit_dict.values():
         profit_list.append(val)
     std = round(np.std(profit_list),2)
-    print("STD: " + str(std))
+    # print("STD: " + str(std))
+    # print("===========================")
+    return result
 
+x_manufacture_level = []
+y_profit = []
+for i in range(100, 250):
+    x_manufacture_level.append(i)
+    y_profit.append(optimal_production_simulation(150, 20, 1000, 150, 28.5, 8.5, i))
 
-optimal_production_simulation(150, 20, 1000, 150, 28.5, 8.5, 160)
+plt.plot(x_manufacture_level, y_profit, 'ro', markersize=1)
+plt.xlabel('Manufacture Level')
+plt.ylabel('Profit')
+plt.show()
