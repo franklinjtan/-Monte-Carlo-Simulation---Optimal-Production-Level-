445323ca7725c4ee3b82af2045ce481778631f1b
Franklin Tan <ft229@cornell.edu>
2022-11-04 15:45:19 -0400

    added user input funcs

--- funcs.py
+++ funcs.py
@@ -8,36 +8,15 @@ import matplotlib.pyplot as plt
 def generate_random_demand(mean, stddev, size):
     """
     Generates a random sample of demand instances from a normal (Gaussian) distribution.
-
-    Parameter mean: the average demand for the product
-    Precondition: mean is an integer
-
-    Parameter stddev: the standard deviation
-    Precondition: stddev is an integer
-
-    Parameter size: the number of instances or random samples to be created
-    Precondition: size is an integer
     """
     demand = np.random.normal(mean, stddev, size)
     for i in range(size):
         demand[i] = round(demand[i],4)
     return demand
 
-def optimal_production_simulation(mean, stddev, size, retail_price, production_cost, disposal_cost, units_manufactured):
+def calc_optimal_production(mean, stddev, size, retail_price, production_cost, disposal_cost, units_manufactured):
     """
     Uses inputs to calculate the optimal production level given randomly generated demand
-
-    Parameter retail_price: the price that the product is being sold for
-    Precondition: retail_price is an integer >= $0.00
-
-    Parameter production_cost: the costs associated with manufacturing the product
-    Precondition: production_cost is an integer >= $0.00
-
-    Parameter disposal_cost: the costs associated with disposing of the product in an environmentally sustainable way
-    Precondition: disposal_cost is an integer >= $0.00
-
-    Parameter units_manufactured: the number that is manufactured as a result of the inputs
-    Precondition: units_manufactured is an integer >= 0
     """
     demand = generate_random_demand(mean, stddev, size)
     profit = retail_price - production_cost
@@ -58,31 +37,50 @@ def optimal_production_simulation(mean, stddev, size, retail_price, production_c
         # Scenario where demand and units manufactured are equal
             profit_dict[demand[x]] = (units_manufactured * profit)
     # print(profit_dict) # Checking what the profit dictionary looks like
-    # print(str(units_manufactured) + " units manufactured")
+    print(str(units_manufactured) + " units manufactured")
 
     # Calculate mean profit of the production level
     sum = 0
     for val in profit_dict.values():
         sum += val
     result = round(sum / len(profit_dict),2)
-    # print("AVG: " + str(result))
+    print("AVG: " + str(result))
 
     # Calculate standard deviation of the profit
     profit_list = []
     for val in profit_dict.values():
         profit_list.append(val)
     std = round(np.std(profit_list),2)
-    # print("STD: " + str(std))
-    # print("===========================")
+    print("STD: " + str(std))
+    print("===========================")
     return result
 
-x_manufacture_level = []
-y_profit = []
-for i in range(100, 250):
-    x_manufacture_level.append(i)
-    y_profit.append(optimal_production_simulation(150, 20, 1000, 150, 28.5, 8.5, i))
+def run_simulation(min_lvl, max_lvl, mean, stddev, size, retail_price, production_cost, disposal_cost):
+    """
+    Simulation run - finds optimal production level by calculating average profit given a demand at various manufacturing levels
+    Plots results on a graph and prints the optimal number of units to manufacture.
+    """
+    x_manufacture_level = []
+    y_profit = []
+    for i in range(min_lvl, max_lvl):
+        x_manufacture_level.append(i)
+        y_profit.append(calc_optimal_production(mean, stdev, size, retail_price, production_cost, disposal_cost, i))
 
-plt.plot(x_manufacture_level, y_profit, 'ro', markersize=1)
-plt.xlabel('Manufacture Level')
-plt.ylabel('Profit')
-plt.show()
+    plt.plot(x_manufacture_level, y_profit, 'ro', markersize=1)
+    plt.xlabel('Manufacture Level')
+    plt.ylabel('Profit')
+    plt.show()
+
+def run_program():
+    min_lvl = str(input("Enter the lowest manufacturing level you want to simulate: "))
+    max_lvl = str(input("Enter the highest manufacturing level you want to simulate: "))
+    mean = str(input("Enter the mean: "))
+    std = str(input("Enter the standard deviation: "))
+    size = str(input("Enter the number of demand simulations you want to run: "))
+    retail_price = str(input("Enter the retail price: "))
+    production_cost = str(input("Enter the production cost: "))
+    disposal_cost = str(input("Enter the disposal cost: "))
+
+    run_simulation(min_lvl, max_lvl, mean, std, size, retail_price, production_cost, disposal_cost)
+
+run_program()
