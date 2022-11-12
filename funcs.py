2938c749e47d67d563940f79d3897a7888e27d32
Franklin Tan <33529408+franklinjtan@users.noreply.github.com>
2022-11-12 11:41:27 -0500

    Updated: added normal and uniform dist

--- funcs.py
+++ funcs.py
@@ -5,20 +5,34 @@ Date: 11/04/22
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generate_random_demand(mean, stddev, size):
+def generate_random_demand_normal(mean, stddev, size):
     """
     Generates a random sample of demand instances from a normal (Gaussian) distribution.
     """
-    demand = np.random.normal(mean, stddev, size)
+    demand = np.random.normal(mean,stddev,size)
     for i in range(size):
         demand[i] = round(demand[i],4)
     return demand
 
-def calc_optimal_production(mean, stddev, size, retail_price, production_cost, disposal_cost, units_manufactured):
+def generate_random_demand_uniform(low, high, size):
+    """
+    Generates a random sample of demand instances from a uniform distribution.
+    """
+    demand = np.random.uniform(low,high,size)
+    for i in range(size):
+        demand[i] = round(demand[i],4)
+    return demand
+print(generate_random_demand_uniform(100,200,1000))
+
+def calc_optimal_production(mean, stddev, size, retail_price, production_cost, disposal_cost, units_manufactured, dist, low, high):
     """
     Uses inputs to calculate the optimal production level given randomly generated demand
     """
-    demand = generate_random_demand(mean, stddev, size)
+    if dist == "n":
+        demand = generate_random_demand_normal(mean, stddev, size)
+    elif dist == "u":
+        demand = generate_random_demand_uniform(low, high, size)
+        
     profit = retail_price - production_cost
     profit_dict = {}
 
@@ -55,7 +69,7 @@ def calc_optimal_production(mean, stddev, size, retail_price, production_cost, d
     print("===========================")
     return result
 
-def run_simulation(min_lvl, max_lvl, mean, stddev, size, retail_price, production_cost, disposal_cost):
+def run_simulation(min_lvl, max_lvl, mean, stddev, size, retail_price, production_cost, disposal_cost, dist, low, high):
     """
     Simulation run - finds optimal production level by calculating average profit given a demand at various manufacturing levels
     Plots results on a graph and prints the optimal number of units to manufacture.
@@ -64,18 +78,19 @@ def run_simulation(min_lvl, max_lvl, mean, stddev, size, retail_price, productio
     y_profit = []
     for i in range(min_lvl, max_lvl):
         x_manufacture_level.append(i)
-        y_profit.append(calc_optimal_production(mean, stddev, size, retail_price, production_cost, disposal_cost, i))
+        y_profit.append(calc_optimal_production(mean, stddev, size, retail_price, production_cost, disposal_cost, i, dist, low, high))
 
     # Print out the recommendation to the user
     optimal_value_profit = max(y_profit)
     optimal_number = x_manufacture_level[y_profit.index(optimal_value_profit)]
     print("Recommendation: " + str(optimal_number) + " units yields an average profit of " + "$" + str(optimal_value_profit))
 
+    # Plotting
     plt.title("Manufacturing Level Simulation", loc = 'left', fontsize = 20)
     plt.xlabel('Manufacture Level')
     plt.ylabel('Profit')
     plt.xticks(np.arange(min(x_manufacture_level), max(x_manufacture_level)+1, 10.0))
-    sctr = plt.scatter(x_manufacture_level, y_profit, c=y_profit, cmap='RdYlGn')
+    sctr = plt.scatter(x_manufacture_level, y_profit, c=y_profit, cmap='RdYlBu')
     plt.colorbar(sctr, format='$%d')
     plt.grid()
     plt.show()
@@ -89,11 +104,11 @@ def run_program():
     retail_price = float(input("Enter the retail price: "))
     production_cost = float(input("Enter the production cost: "))
     disposal_cost = float(input("Enter the disposal cost: "))
+    dist = input("Enter the 'n' for normal distribution or 'u' for uniform distribution: ")
+    low = int(input("Enter the low only for uniform distribution. Enter 0 for normal distribution:  "))
+    high = int(input("Enter the high only for uniform distribution. Enter 0 for normal distribution: "))
 
-    run_simulation(min_lvl, max_lvl, mean, stddev, size, retail_price, production_cost, disposal_cost)
-
-# Testing Function: generate_random_demand(mean, stddev, size)
-# print(generate_random_demand(150, 20, 1000))
+    run_simulation(min_lvl, max_lvl, mean, stddev, size, retail_price, production_cost, disposal_cost, dist, low, high)
 
 # Enable to run program
 run_program()
